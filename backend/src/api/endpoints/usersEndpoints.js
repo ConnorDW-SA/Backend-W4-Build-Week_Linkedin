1962f16aa8cf3bd5ff2f28e4044b63d1d2665da7
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-01-23 11:34:49 +0200

    user endpoints and model done

--- backend/src/api/endpoints/usersEndpoints.js
+++ backend/src/api/endpoints/usersEndpoints.js
@@ -1,8 +1,78 @@
 import express from "express";
-const router = express.Router();
+import usersModel from "../routes/usersModel.js";
+import createHttpError from "http-errors";
 
-router.get("/users", (req, res) => {
-    return res.send({ message: "OK" });
+const userRouter = express.Router();
+
+// GET /users
+
+userRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await usersModel.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
 });
 
-export default router;
+// GET /users/:id
+
+userRouter.get("/:id", async (req, res, next) => {
+  try {
+    const user = await usersModel.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      next(createHttpError(404, `User with id ${req.params.id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /users
+
+userRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new usersModel(req.body);
+    const { _id } = await newUser.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /users/:id
+
+userRouter.put("/:id", async (req, res, next) => {
+  try {
+    const user = await usersModel.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true
+    });
+    if (user) {
+      res.send(user);
+    } else {
+      next(createHttpError(404, `User with id ${req.params.id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /users/:id
+
+userRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const user = await usersModel.findByIdAndDelete(req.params.id);
+    if (user) {
+      res.status(204).send();
+    } else {
+      next(createHttpError(404, `User with id ${req.params.id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default userRouter;
