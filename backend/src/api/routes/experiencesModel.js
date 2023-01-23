aac470bf9e1ed92a8f0e69f4d7a169a41d1c4aed
Morgan <morgan@morganevans.dev>
2023-01-23 09:40:19 +0000

    added embed

---
+++ backend/src/api/routes/experiencesModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const experiencesModel = new Schema({
+    role: {
+        type: String,
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        default: null,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    area: {
+        type: String,
+        default: "None given",
+    },
+    image: {
+        type: String,
+        default: "https://via.placeholder.com/256x256",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default model("Experience", experiencesModel);
