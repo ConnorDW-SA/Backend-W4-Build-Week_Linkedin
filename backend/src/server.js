be9e574123590b30a489fde77c3da5d424e4ed92
Morgan <morgan@morganevans.dev>
2023-01-25 14:01:25 +0000

    added signup logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,15 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import {
-  badRequestHandler,
-  genericServerErrorHandler,
-  notFoundHandler,
-  unauthorizedHandler
+    badRequestHandler,
+    genericServerErrorHandler,
+    notFoundHandler,
+    unauthorizedHandler,
 } from "./errorHandlers.js";
 import postsRouter from "./api/endpoints/postEndpoints.js";
 import userRouter from "./api/endpoints/usersEndpoints.js";
 import experienceRouter from "./api/endpoints/experiencesEndpoints.js";
+import authRouter from "./api/endpoints/auth.js";
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -24,6 +25,7 @@ server.use(express.json());
 server.use("/posts", postsRouter);
 server.use("/users", userRouter);
 server.use("/users", experienceRouter);
+server.use("/auth", authRouter);
 
 server.use(badRequestHandler);
 server.use(unauthorizedHandler);
@@ -33,9 +35,9 @@ server.use(genericServerErrorHandler);
 mongoose.connect(process.env.MONGO_URL);
 
 mongoose.connection.on("connected", () => {
-  console.log("Successfully connected to Mongo!");
-  server.listen(port, () => {
-    console.table(listEndpoints(server));
-    console.log(`Server is running on port ${port}`);
-  });
+    console.log("Successfully connected to Mongo!");
+    server.listen(port, () => {
+        console.table(listEndpoints(server));
+        console.log(`Server is running on port ${port}`);
+    });
 });
