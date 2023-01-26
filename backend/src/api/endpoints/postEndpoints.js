cdf9a04f4487323cef6f2172ee3418be063d153a
Damian Olszewski <damianolszewski25@gmail.com>
2023-01-26 11:01:27 +0100

    works ig

--- backend/src/api/endpoints/postEndpoints.js
+++ backend/src/api/endpoints/postEndpoints.js
@@ -10,105 +10,93 @@ const postsRouter = express.Router();
 //POST
 
 postsRouter.post("/", async (req, res, next) => {
-    try {
-        const newPost = new postsModel(req.body);
-        const { _id } = await newPost.save();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const newPost = new postsModel(req.body);
+    console.log(req.body);
+    const doc = await newPost.save();
+
+    res.send(doc);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //GET
 
 postsRouter.get("/", async (req, res, next) => {
-    try {
-        const mongoQuery = q2m(req.query);
-        const total = await postsModel.countDocuments(mongoQuery.criteria);
-        console.log(total);
-        const posts = await postsModel
-            .find(mongoQuery.criteria, mongoQuery.options.fields)
-            .sort(mongoQuery.options.sort)
-            .skip(mongoQuery.options.skip)
-            .limit(mongoQuery.options.limit)
-            .populate("user");
+  try {
+    const mongoQuery = q2m(req.query);
+    const total = await postsModel.countDocuments(mongoQuery.criteria);
+    console.log(total);
+    const posts = await postsModel
+      .find(mongoQuery.criteria, mongoQuery.options.fields)
+      .sort(mongoQuery.options.sort)
+      .skip(mongoQuery.options.skip)
+      .limit(mongoQuery.options.limit)
+      .populate("user");
 
-        res.status(200).send({
-            links: mongoQuery.links(total),
-            total,
-            totalPages: Math.ceil(total / mongoQuery.options.limit),
-            posts,
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).send({
+      links: mongoQuery.links(total),
+      total,
+      totalPages: Math.ceil(total / mongoQuery.options.limit),
+      posts,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // GET SPECIFIC
 
 postsRouter.get("/:postId", async (req, res, next) => {
-    try {
-        const posts = await postsModel.findById(req.params.postId);
+  try {
+    const posts = await postsModel.findById(req.params.postId);
 
-        if (posts) {
-            res.send(posts);
-        } else {
-            next(
-                createHttpError(
-                    404,
-                    `post with id ${req.params.postId} not found`
-                )
-            );
-        }
-    } catch (error) {
-        next(error);
+    if (posts) {
+      res.send(posts);
+    } else {
+      next(createHttpError(404, `post with id ${req.params.postId} not found`));
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // PUT
 
 postsRouter.put("/:postId", async (req, res, next) => {
-    try {
-        const updatedPost = await postsModel.findByIdAndUpdate(
-            req.params.postId,
-            req.body,
-            { new: true, runValidators: true }
-        );
+  try {
+    const updatedPost = await postsModel.findByIdAndUpdate(
+      req.params.postId,
+      req.body,
+      { new: true, runValidators: true }
+    );
 
-        if (updatedPost) {
-            res.send(updatedPost);
-        } else {
-            next(
-                createHttpError(
-                    404,
-                    `post with id ${req.params.postId} not found`
-                )
-            );
-        }
-    } catch (error) {
-        next(error);
+    if (updatedPost) {
+      res.send(updatedPost);
+    } else {
+      next(createHttpError(404, `post with id ${req.params.postId} not found`));
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // DELETE
 
 postsRouter.delete("/:postId", async (req, res, next) => {
-    try {
-        const deletedPost = await postsModel.findByIdAndDelete(
-            req.params.postId
-        );
-        if (deletedPost) {
-            res.status(204).send();
-        } else {
-            next(
-                createHttpError(
-                    404,
-                    `posts with id ${req.params.postId} not found`
-                )
-            );
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const deletedPost = await postsModel.findByIdAndDelete(req.params.postId);
+    if (deletedPost) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(404, `posts with id ${req.params.postId} not found`)
+      );
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default postsRouter;
--- backend/src/api/routes/postsModel.js
+++ backend/src/api/routes/postsModel.js
@@ -3,21 +3,20 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const postsSchema = new Schema(
-    {
-        text: { type: String, required: true },
-        username: { type: String, required: true },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-        image: {
-            type: String,
-            required: false,
-        },
+  {
+    text: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    {
-        timestamps: true,
-    }
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export default model("posts", postsSchema);
